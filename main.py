f8af558a225ec2eb0f2b0f6126683913fda0d4ee
Sergienko Anton <sergienkoanton@mail.ru>
2024-10-24 18:26:37 +0300

    Change main part

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-count = 0
+count2 = 0
 for i in range(10, 100):
     if not (not(i % 2 == 0) and (i % 13 == 0)):
+            if not (not(i % 2 == 0) and (i % 13 == 0)):
+                    if not (not(i % 2 == 0) and (i % 13 == 0)):
+                            if not (not(i % 2 == 0) and (i % 13 == 0)):
+                                    if not (not(i % 2 == 0) and (i % 13 == 0)):
+                                            if not (not(i % 2 == 0) and (i % 13 == 0)):
+                                                
         count+=1
 print(count)
